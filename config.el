20e8f43b4afc0d9a73e3aecd0bc07e107f1b4c64
Kari Oikarinen <kluge@kluge.fi>
2022-08-03 15:48:52 +0300

    Add inserting date with <f5>

--- config.el
+++ config.el
@@ -4,3 +4,12 @@
 
 (setq display-line-numbers-type nil)
 (setq org-directory "~/org/")
+
+(defun kluge-insert-date (arg)
+  (interactive "P")
+  (insert (cond
+           ((not arg) (format-time-string "%Y-%m-%d"))
+           ((equal arg '(4)) (format-time-string "%Y-%m-%dT%H:%M"))
+           ((equal arg '(16)) (format-time-string "%Y-%m-%d %H:%M")))))
+
+(map! :ni "<f5>" 'kluge-insert-date)
